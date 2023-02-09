e5bf4118d0bd49c57a362529d30a4f085479b5f0
Giacomoponti <giacomoponti00@gmail.com>
2023-02-09 17:45:40 +0100

    message

--- trial1/fibonacci.js
+++ trial1/fibonacci.js
@@ -1,34 +1,7 @@
-function fibonacci(n) {
-    if (n < 2) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-
-    }   
-    console.log(fibonacci(10));
-class TreeNode {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+function months_passed_since_year_0() {
+    var d = new Date();
+    var y = d.getFullYear();
+    var m = d.getMonth();
+    return y * 12 + m;
 }
-class avlTree {
-    root = null;
-    constructor() {
-        this.root = null;
-    }
-    add(value) {
-        this.root = this.addHelper(this.root, value);
-    } 
-    addHelper(root, value) {
-        if (root == null) {
-            return new TreeNode(value);
-        }
-        if (value < root.value) {
-            root.left = this.addHelper(root.left, value);
-        } else {
-            root.right = this.addHelper(root.right, value);
-        }
-        return root;
-    }
-        
-}
+console.log(months_passed_since_year_0());
